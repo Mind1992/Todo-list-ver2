e307e7aca6bc55072b746778ea2acd5ae77af4f4
Sergii Mertsalov & Amy Vaillancourt-Sals <student@epicodus.com>
2014-07-15 11:42:23 -0700

    added layout and functionality foundations

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+  $("form#lists").submit(function(event) {
+  event.preventDefault();
 
+  var inputtedListName = $("input#inputtedNewList").val();
+  var newList = {name: inputtedListName };
+  console.log(newList);
+  $("div#new-lists ul").append("<li>" + newList.name + "</li>");
+
+  $("input#inputtedNewList").val("");
+
+  });
 });
