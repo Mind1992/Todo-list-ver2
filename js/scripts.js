592b866645920163382de58a19bcccccb8073b3e
Sergii Mertsalov & Amy Vaillancourt-Sals <student@epicodus.com>
2014-07-15 16:55:49 -0700

    Mac fixed jquery list  bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,38 @@
 $(document).ready(function() {
+  var currentList;
+
   $("form#lists").submit(function(event) {
     event.preventDefault();
 
     var inputtedListName = $("input#inputtedNewList").val();
     var newList = {name: inputtedListName, tasks:[]};
-
+    console.log(newList)
     $("div#new-lists ul").append("<li><span>" + newList.name + "</span></li>");
+    currentList = newList;
 
     $("input#inputtedNewList").val("");
 
+    $("#new-lists ul li").last().click(function(){
+      $("#new-tasks").show();
+      $("#inputtedTaskName").text("<h4>" + newList.name + "</h4>");
+      currentList = newList;
+      $("#new-tasks ul").empty();
+      currentList.tasks.forEach(function(task) {
+        $("#new-tasks ul").append("<li>" + task.name + "</li>");
+      })
+    });
+  });
 
   $("form#tasks").submit(function(event) {
+    alert('im submitting');
     event.preventDefault();
 
     var inputtedTask = $("input#inputtedTaskName").val();
-    var newTasks = {task: inputtedTask };
-    console.log(newTasks);
-    $("div#new-tasks ul").append("<li>" + newTasks.task + "</li>");
+    var newTask = {name: inputtedTask };
+    console.log(newTask);
+    $("div#new-tasks ul").append("<li>" + newTask.name + "</li>");
 
     $("input#inputtedTaskName").val("");
-    newList.tasks.push(newTasks.task);
-
-
-  $("#new-lists ul").last().click(function(){
-    $("#new-tasks ul").show();
-      });
-    });
+    currentList.tasks.push(newTask);
   });
 });
